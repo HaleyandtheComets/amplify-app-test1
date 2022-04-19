817420572a16ec87252361a8544cde212b123240
HaleyandtheComets <martin.haley.github@haleyandthecomets.com>
2022-04-19 16:44:52 +0000

    changed react to haleyandthecomets

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          HaleyandtheComets.com is Coming Soon!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://haleyandthecomets.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Stay Tuned
         </a>
       </header>
     </div>
